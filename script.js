6563238164eb6e0920452e1efa7086b852e4db52
Abdul Bundu Kamara <bunduka2@gmail.com>
2018-07-01 19:50:52 -0400

    adding runTheClock function and Setsetinterval
    Setting the sec, min and hr to move simultaneously:

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let sePosition = 267;
+	var date = new Date();
+	console.log(date);
 
-var date = new Date();
-console.log(date);
+	let hr = date.getHours();
+	let min = date.getMinutes();
+	let sec = date.getSeconds();
+	console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + sePosition + "deg)";
+	let hrPosition = (hr*360/12)+(min*(360/60)/12);
+	let minPosition = (min*360/60)+(min*(360/60)/60);
+	let secPosition =  sec*360/60;
+
+
+function runTheClock() {
+	// body...
+	hrPosition = hrPosition+(3/360);
+	minPosition = minPosition+(6/60);
+	secPosition = secPosition+6;
+
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000)
